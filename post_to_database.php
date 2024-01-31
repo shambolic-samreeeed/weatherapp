837070bae61c3e14147e29c2f9af8d24e01a5911
shambolic-samreeeed <154119074+shambolic-samreeeed@users.noreply.github.com>
2024-01-31 20:45:51 +0545

    Update post_to_database.php

--- post_to_database.php
+++ post_to_database.php
@@ -1,14 +1,14 @@
 <?php
-$servername="localhost";
-$username="root";
-$password="";
-$dbname="city_weather";
-$tableName="dibrugarh";
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "city_weather";
+$tableName = "dibrugarh";
 
-$conn= new mysqli($servername, $username, $password, $dbname);
+$conn = new mysqli($servername, $username, $password, $dbname);
 
-if ($conn->connect_error){
-    die ("Connection failed " . $conn->connect_error);
+if ($conn->connect_error) {
+    die("Connection failed " . $conn->connect_error);
 }
 
 $day_of_week = $_POST['day_of_week'];
@@ -21,7 +21,7 @@ $humidity = $_POST['humidity'];
 $pressure = $_POST['pressure'];
 
 
-$insert_sql = "INSERT INTO $tableName (Day_of_Week, Day_and_Date, Weather_Condition, Weather_Icon, Temperature, Pressure, Wind_Speed, Humidity, City) VALUES ('$day_of_week', '$day_and_date', '$weather_condition', '$weather_icon', '$temperature', '$pressure', '$wind_speed', '$humidity')";
+$insert_sql = "INSERT INTO $tableName (Day_of_Week, Day_and_Date, Weather_Condition, Weather_Icon, Temperature, Pressure, Wind_Speed, Humidity) VALUES ('$day_of_week', '$day_and_date', '$weather_condition', '$weather_icon', '$temperature', '$pressure', '$wind_speed', '$humidity')";
 if ($conn->query($insert_sql) !== TRUE) {
     echo "Error: " . $insert_sql . "<br>" . $conn->error;
 } else {
